cab8c867336b2ab5ba22da3e0ae2f1c4ed85eef0
dansonmwangi <danson.mwangi@craftsilicon.com>
2026-03-05 13:05:57 +0300

    center loan

--- kairo-ui/Services/OldApiDBConstants.cs
+++ kairo-ui/Services/OldApiDBConstants.cs
@@ -3,5 +3,14 @@
     public static class OldApiDBConstants
     {
         public const string GET_SEARCHRESULT = "p_GetSearchResult";
+
+        // Microfinance / Center Loan Scheme
+        public const string GET_GROUP_LOAN_SCHEMES = "p_GetGroupLoanSchemes";
+        public const string ADD_EDIT_GROUP_LOAN_SCHEMES = "p_AddEditGroupLoanSchemes";
+        public const string DELETE_GROUP_LOAN_SCHEMES = "p_DeleteGroupLoanSchemes";
+        public const string GET_GROUP_LOAN_MENU = "p_GetGroupLoanMenu";
+        public const string ADD_EDIT_GROUP_LOAN_MENU = "p_AddEditGroupLoanMenu";
+        public const string GET_GROUP_LOAN_SCHEME_PRODUCTS = "p_GetGroupLoanSchemeProducts";
+        public const string ADD_EDIT_GROUP_LOAN_SCHEME_PRODUCTS = "p_AddEditGroupLoanSchemeProducts";
     }
 }
